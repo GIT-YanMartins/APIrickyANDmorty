1847b5e7451f93e609f6242483d41015a4799a0f
GIT-YanMartins <yangabriel15@outlook.com>
2023-03-25 15:57:52 -0300

    Primeiras alteracoes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = axios.create({
+  baseURL: "https://rickandmortyapi.com/api/character"
+});
+
+export default class App extends React.Component {
+  state = {
+    info: []
+  };
+  pegarPersonagens = async () => {
+    const resposta = await API.get();
+    console.log(resposta);
+
+    const itensApi = resposta.data.results.map((item) => {
+      return {
+        ...item
+      };
+    });
+
+    this.setState({
+      info: itensApi
+    });
+  };
+
+  componentDidMount() {
+    this.pegarPersonagens();
+  }
+
+  render() {
+    return (
+      <>
+        <h1>API RICKY AND MORTY</h1>
+        {this.state.info.map((item) => (
+          <>
+            <h2>{item.name}</h2>
+            <img src={item.image} alt="" />
+            <p>{item.species}</p>
+          </>
+        ))}
+      </>
+    );
+  }
 }
-
-export default App;
